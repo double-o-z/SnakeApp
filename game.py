85945975c9be09a424f7f3628159ba9f7457eb62
or-ohev-zion <or.ohev.zion@gmail.com>
2016-02-01 12:52:08 +0200

    snake movement now stable. also game will end with specific print for each scenario.

--- game.py
+++ game.py
@@ -2,56 +2,73 @@ __author__ = 'OR'
 import random
 import signal
 import time
+import sys
 
-from snake import Snake
+sys.path.append("/Users/or/dev/Snake")
 
-CELLS = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
-         (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
-         (2, 0), (2, 1), (2, 2), (2, 3), (2, 4),
-         (3, 0), (3, 1), (3, 2), (3, 3), (3, 4),
-         (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
+from snake import Snake, CELLS
 
 
 class Game:
     def __init__(self):
         self.snake = Snake()
-        self.snake.body.append(random.choice(CELLS))
+        self.snake.create_snake(random.choice(CELLS))
         signal.signal(signal.SIGALRM, self.raise_exception)
 
     def raise_exception(self, sig_num, stack_frame):
         raise KeyboardInterrupt
 
     def start_game(self):
-        print("""
-        'Welcome to the Snake Jungle v1.0'
-        Choose a direction by it's letter:
-        [A]LEFT, [W]UP, [S]DOWN, [D]RIGHT And Press Enter.
-        The Game will begin in 2 seconds.
-        """)
+        message = \
+            """
+Welcome to the Snake Jungle v1.0
+Enter a direction by it's letter, then press the Enter Key:
+[A]LEFT, [W]UP, [S]DOWN, [D]RIGHT And Press Enter.
+The Game will begin in 2 seconds.
+"""
+        print(message)
         time.sleep(2)
         message = ''
         while True:
+            if self.snake.length >= len(CELLS):
+                message = "Good Game, You WIN."
+                print(message)
+                score_int = 5 * len(self.snake.body)
+                score = """
+Score: {}
+""".format(score_int)
+                print(score)
+                exit(1)
             self.draw_jungle()
             try:
-                signal.alarm(3)
+                signal.alarm(2)
                 self.snake.get_direction()
                 signal.alarm(0)
             except KeyboardInterrupt:
-                continue
-            if self.snake.length >= len(CELLS):
-                message = "Congratz! You've Won!!!"
-                break
-            elif self.snake.body[-1] in self.snake.body[:-1]:
-                message = "Oops.. You ate yourself. Try again."
+                self.snake.grow_snake()
+            if self.snake.body[-1] in self.snake.body[:-1]:
+                message = \
+                    """
+Oops..
+You ate yourself.
+Game Over.
+"""
                 break
             else:
                 continue
         print(message)
+        score_int = 5 * len(self.snake.body)
+        score = """
+Score: {}
+""".format(score_int)
+        print(score)
         exit(0)
 
     def draw_jungle(self):
-        print('\n{}'.format('='*20))
-        print(' _ _ _ _ _')
+        seperator = '\n{}'.format('=' * 20)
+        roof = ' _ _ _ _ _'
+        print(seperator)
+        print(roof)
         tile = '|{}'
         for idx, cell in enumerate(CELLS):
             content = '_'
@@ -59,15 +76,15 @@ class Game:
                 content = 'S'
                 if cell == self.snake.body[-1]:
                     content = 'O'
-            if idx+1 in [5, 10, 15, 20, 25]:
+            if idx + 1 in [5, 10, 15, 20, 25]:
                 print_line += tile.format(content + '|')
                 print(print_line)
-            elif idx+1 in [1, 6, 11, 16, 21]:
+            elif idx + 1 in [1, 6, 11, 16, 21]:
                 print_line = ''
                 print_line += tile.format(content)
             else:
                 print_line += tile.format(content)
+        print(seperator)
 
-        print('\n{}'.format('='*20))
 
 Game().start_game()
--- snake.py
+++ snake.py
@@ -1,28 +1,79 @@
 __author__ = 'OR'
 import random
 
-DIRECTIONS = 'AWSD'
+CELLS = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
+         (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
+         (2, 0), (2, 1), (2, 2), (2, 3), (2, 4),
+         (3, 0), (3, 1), (3, 2), (3, 3), (3, 4),
+         (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
+
+DIRECTIONS = {
+    'A': {
+        'Direction': 'Left',
+        'X-Axis': -1,
+        'Y-Axis': 0,
+    },
+    'W': {
+        'Direction': 'Up',
+        'X-Axis': 0,
+        'Y-Axis': -1,
+    },
+    'S': {
+        'Direction': 'Down',
+        'X-Axis': 0,
+        'Y-Axis': 1,
+    },
+    'D': {
+        'Direction': 'Right',
+        'X-Axis': 1,
+        'Y-Axis': 0,
+    }
+}
 
 
-class Snake():
+class Snake:
     def __init__(self):
-        self.length = 1
+        self.length = 0
         self.apples = 0
-        self.current_direction = DIRECTIONS[random.randint(0, 3)]
+        self.current_direction = None
+        self.current_head = None
         self.body = []
-        self.user_input = ''
+        self.user_input = None
 
     def get_direction(self):
         print('Insert Move: ')
-        self.user_input = raw_input().upper()
-        if self.user_input:
-            if self.user_input in DIRECTIONS:
-                if self.user_input == 'A':
-                    print('You Move Left')
-                elif self.user_input == 'W':
-                    print('You Move UP')
-                elif self.user_input == 'S':
-                    print('You Move Down')
-                elif self.user_input == 'D':
-                    print('You Move Right')
+        user_input = raw_input().upper()
+        if user_input:
+            self.user_input = user_input
+            if self.user_input in DIRECTIONS.keys():
+                self.grow_snake()
+            elif self.user_input == 'Q':
+                print("Game Over")
+                exit(1)
         return
+
+    def grow_snake(self):
+        self.current_head = self.body[-1]
+        self.current_direction = DIRECTIONS.get(self.user_input)
+        self.current_head = (self.current_head[0] + self.current_direction['Y-Axis'],
+                             self.current_head[1] + self.current_direction['X-Axis'])
+        if self.current_head not in CELLS:
+            seperator = '\n{}'.format('=' * 20)
+            print(seperator)
+            print("\n\nBumped the wall. Game Over.\n")
+            score_int = 5 * len(self.body)
+            score = """
+Score: {}
+""".format(score_int)
+            print(score)
+            print(seperator)
+            exit(1)
+        self.body.append(self.current_head)
+        self.length += 1
+
+    def create_snake(self, tile):
+        self.body.append(tile)
+        self.length += 1
+        self.current_head = self.body[0]
+        self.current_direction = random.choice(DIRECTIONS.keys())
+        self.user_input = self.current_direction
